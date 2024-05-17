33454bbbaad1e23d3b529d383eff8cdcd7a6ce0a
shrutikmali <70632906+shrutikmali@users.noreply.github.com>
2024-05-17 11:46:02 +0530

    Testing webhook

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	// Comment to test github webhook (1)
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
